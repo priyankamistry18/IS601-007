0911890dc9fff8116d22ab5bb644ea368f20b1b8
priyankamistry18 <60395460+priyankamistry18@users.noreply.github.com>
2022-11-03 21:35:24 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import sys
+jls_extract_var = flask
+from jls_extract_var import Flask, request
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+
+def hello_world():
+
+    name = request.args.get('name', 'World')
+
+    return f'Hello {name}!'
+
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
