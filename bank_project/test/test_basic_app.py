8e7fa9b865fc60c138d57a152aaf2a5cbb6d3172
priyankamistry18 <60395460+priyankamistry18@users.noreply.github.com>
2022-12-22 18:55:56 -0500

    Update test_basic_app.py

--- bank_project/test/test_basic_app.py
+++ bank_project/test/test_basic_app.py
@@ -26,7 +26,3 @@ def client(app):
 def runner(app):
     return app.test_cli_runner()
 
-
-def test_request_example(client):
-    response = client.get("/")
-    assert b"Hello World!" in response.data
