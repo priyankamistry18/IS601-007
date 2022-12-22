a206226ee76d4363132a1acb6b51717beca5f8f5
priyankamistry18 <60395460+priyankamistry18@users.noreply.github.com>
2022-12-22 18:44:27 -0500

    Delete bank_project directory

--- bank_project/Project/bank_project/views/hello.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request, redirect, url_for
-hello = Blueprint('hello', __name__, url_prefix='/')
-
-
-@hello.route('/')
-def index():    
-    return redirect(url_for("auth.landing_page"))
